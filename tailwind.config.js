2152d923c15016a851147d261fb42300c8059e17
mac fitton <macfittondev@gmail.com>
2022-10-10 11:47:14 -0700

    update

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["public/views"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "Poppins": "Poppins, sans-serif"
+      }
+    },
+  },
+  plugins: [],
+}
